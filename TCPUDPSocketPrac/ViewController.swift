72b81b11815687265069aaee28ae6536ca16446a
Charles Cheng <cowton0517@gmail.com>
2022-11-28 09:20:59 +0800

    Initial Commit

---
+++ TCPUDPSocketPrac/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TCPUDPSocketPrac
+//
+//  Created by Chun-Li Cheng on 2022/11/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
